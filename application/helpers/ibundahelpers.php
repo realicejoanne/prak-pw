f577cf949354c64e812b6d932f32f096e23df517
realicejoanne <alicetheuglyduckling@gmail.com>
2017-12-12 11:52:28 +0700

    hahahahanjir

--- application/helpers/ibundahelpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function hashpassword($password = '')
-    {
-        
-        return hash('sha1', $password . config_item('encryption key'));
-    }
--- application/views/layout.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$this->load->view('component/header');
-$this->load->view($content);
-$this->load->view('component/footer');
