2e185cf6e533ea0bab25818a642ede5a866c9020
realicejoanne <alicetheuglyduckling@gmail.com>
2017-12-18 00:46:11 +0700

    dead.

--- application/views/page/profile/insert.php
+++ application/views/page/profile/insert.php
@@ -11,28 +11,27 @@ $this->load->view('component/menu_submission');
     <?php endif;?>
     <div class="container">
     <?php echo form_open()?>
-            <br><div style="border-radius: 25px;border: 2px solid #a8a8a8;padding: 20px;margin: auto;">
-                <center><h3>ADD ARTICLE</h3></center><br>
-                <label>Judul </label>
-                <input type="text" name="title"><br>
-                <label>Gambar </label>
-                <input type="text" name="picture"><br>
-				<label>Tingkat </label>
-                	<select name="stage">
-						<option value="">Select</option>
+            <div class="insert" style="overflow: hidden; border: 1px solid grey; padding: 10px 20px; margin-top: 20px">
+                <h4>ARTICLE</h4>
+                <label>Tingkat</label>
+                	<select name="stage" class="form-control">
                         <option value="0-6 bulan">0-6 bulan</option>
                         <option value="7-11 bulan">7-11 bulan</option>
                         <option value="1-3 tahun">1-3 tahun</option>
                         <option value="ibu">ibu</option>
-                    </select><br>
-                <label>Kategori </label>
-                	<select name="topic">
-						<option value="">Select</option>
+                    </select><br />
+                <table>Kategori</table>
+                	<select name="topic" class="form-control">
                         <option value="kesehatan">kesehatan</option>
                         <option value="tips & tricks">tips & tricks</option>
                         <option value="mitos">mitos</option>
-                    </select><br><br>
-                <textarea rows="5" cols="200" name="content"></textarea><br><br>
+                    </select><br />
+        		<label>Judul</label>
+                <input type="text" name="title"/><br />
+                <label>Gambar</label>
+                <input type="text" name="picture"/><br />
+                <label>Artikel</label>
+                <textarea rows="4" cols="50" name="content"></textarea><br />
                 <input type="submit" name="submit" value="Submit"/>
             </div>
 	<?php echo form_close()?>
